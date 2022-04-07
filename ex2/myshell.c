4765651de8e53cb9adf7ad7a1dd8eb6aa36ee8f6
Dvir Golan <dvirgol10@gmai.com>
2022-04-07 23:30:13 +0300

    Fix error handling in second assignment

--- ex2/myshell.c
+++ ex2/myshell.c
@@ -19,24 +19,31 @@ void print_error_message_and_exit(void) {
 }
 
 
-int handle_waitpid(int pid) {
+int handle_waitpid(int pid, int exit_on_error) {
 	if (waitpid(pid, NULL, 0) == -1) { // waitpid failed
 		if (errno != ECHILD && errno != EINTR) { // those aren't considered as actual errors in our definition 
-			print_error_message_and_exit();
+			if (exit_on_error) {
+				print_error_message_and_exit();
+			} else {
+				print_error_message();
+				return -1; // '-1' means that there was a "fatal" error 
+			}
 		}
-		return 0;
+		return 1; // '1' means that there was an error, but not an actual error that requires exiting the shell
 	}
-	return 1;
+	return 0; // '0' means that everything is OK
 }
 
 
 void SIGCHLDHandler(int signum, siginfo_t *info, void *ptr) { // its formal purpose is to remove zombies
-	handle_waitpid(info->si_pid);
+	handle_waitpid(info->si_pid, 1);
 }
 
 
 int prepare(void) {
-	signal(SIGINT, SIG_IGN); // ignore SIGINT, that is not terminate upon SIGINT
+	if (signal(SIGINT, SIG_IGN) == SIG_ERR) { // ignore SIGINT, that is not terminate upon SIGINT
+		return 1;
+	}
 	struct sigaction newActionForSIGCHLD = {
 	  .sa_sigaction = SIGCHLDHandler,
 	  .sa_flags = SA_SIGINFO | SA_RESTART
@@ -49,18 +56,31 @@ int prepare(void) {
 
 
 int finalize(void) {
-	while (handle_waitpid(-1)); // wait for all the child processes (more precisely, the background ones) to end (in order to remove zombies). Note that waitpid(-1, ...) is equivalent to wait(...);
-	return 0;
+	// wait for all the child processes (more precisely, the background ones) to end (in order to remove zombies). Note that waitpid(-1, ...) is equivalent to wait(...);
+	while (1) {
+		int val = handle_waitpid(-1, 0);
+		if (val == -1) { // waitpid failed
+			return 1;
+		}
+		if (val == 1) { // there aren't child zombies anymore
+			return 0;
+		}
+	}
+	return 0; // dead code
 }
 
 
 void register_foreground_signal_handler(void) {
-	signal(SIGINT, SIG_DFL); // terminate upon SIGINT
+	if (signal(SIGINT, SIG_DFL) == SIG_ERR) { // terminate upon SIGINT
+		print_error_message_and_exit();
+	}
 }
 
 
 void register_background_signal_handler(void) {
-	signal(SIGINT, SIG_IGN); // ignore SIGINT, that is not terminate upon SIGINT
+	if (signal(SIGINT, SIG_IGN) == SIG_ERR) { // ignore SIGINT, that is not terminate upon SIGINT
+		print_error_message_and_exit();
+	}
 }
 
 
@@ -100,7 +120,10 @@ int exeucte_regular(int count, char** arglist) {
 		register_foreground_signal_handler();
 		execute_command(arglist);
 	} else { // parent process
-		handle_waitpid(pid);
+		if (handle_waitpid(pid, 0) == -1) { // waitpid failed
+			print_error_message();
+			return 0;
+		}
 	}
 	return 1;
 }
@@ -130,33 +153,63 @@ int execute_piping(int count, char** arglist, int pipe_index) {
 	}
 	pid_t pid_first = fork();
 	if (pid_first == -1) { // fork failed
-		close(pipefd[0]);
-		close(pipefd[1]);
 		print_error_message();
+		if (close(pipefd[0]) == -1) {
+			print_error_message();
+			return 0;
+		}
+		if (close(pipefd[1]) == -1) {
+			print_error_message();
+			return 0;
+		}
 		return 0;
 	} else if (pid_first == 0) { // child process
 		register_foreground_signal_handler();
-		close(pipefd[0]); // this process is the write end, it sends data to the second child
-		dup2(pipefd[1], STDOUT_FILENO); // now the stdout will be the write end of the pipe
+		if (close(pipefd[0]) == -1) { // this process is the write end, it sends data to the second child
+			print_error_message_and_exit();
+		}
+		if (dup2(pipefd[1], STDOUT_FILENO) == -1) { // now the stdout will be the write end of the pipe
+			print_error_message_and_exit();
+		} 
 		arglist[pipe_index] = NULL; // override the "|" sign with NULL because we do not pass this as an argument to execvp, and want to designate that this is the end of the first command
 		execute_command(arglist);
 	} else { // parent process
-		close(pipefd[1]); // there is no more need for the write end
+		if (close(pipefd[1]) == -1) { // there is no more need for the write end
+			print_error_message();
+			return 0;
+		}
 		pid_t pid_second = fork();
 		if (pid_second == -1) { // fork failed
-			close(pipefd[0]); // close its end - the read end
-			handle_waitpid(pid_first); // wait for the first child process to terminate
 			print_error_message();
+			if (close(pipefd[0]) == -1) { // close its end - the read end
+				print_error_message();
+				return 0;
+			}
+			// wait for the first child process to terminate
+			if (handle_waitpid(pid_first, 0) == -1) { // waitpid failed
+				print_error_message();
+				return 0;
+			}
 			return 0;
 		} else if (pid_second == 0) { // child process
 			register_foreground_signal_handler();
-			close(pipefd[1]); // this process is the read end, it reads data from the first child
-			dup2(pipefd[0], STDIN_FILENO); // now the stdin will be the read end of the pipe
+			if (dup2(pipefd[0], STDIN_FILENO) == -1) { // now the stdin will be the read end of the pipe
+				print_error_message_and_exit();
+			} 
 			execute_command(arglist + pipe_index + 1); // the beginning of the second command is right after the "|" sign.
 		} else { // parent process
-			close(pipefd[0]);
-			handle_waitpid(pid_first);
-			handle_waitpid(pid_second);
+			if (close(pipefd[0]) == -1) {
+				print_error_message();
+				return 0;
+			}
+			if (handle_waitpid(pid_first, 0) == -1) { // waitpid failed
+				print_error_message();
+				return 0;
+			}
+			if (handle_waitpid(pid_second, 0) == -1) { // waitpid failed
+				print_error_message();
+				return 0;
+			}
 		}
 	}
 	return 1;
@@ -168,19 +221,31 @@ int execute_output_redirection(int count, char** arglist) {
 	int fd = open(arglist[count - 1], O_APPEND  | O_CREAT | O_WRONLY, S_IRWXU);
 	if (fd == -1) { // open failed
 		print_error_message();
-		return 0;	}
+		return 0;
+	}
 	if (pid == -1) { // fork failed
-		close(fd);
 		print_error_message();
+		if (close(fd) == -1) {
+			print_error_message();
+			return 0;
+		}
 		return 0;
 	} else if (pid == 0) { // child process
 		register_foreground_signal_handler();
-		dup2(fd, STDOUT_FILENO); // now the stdout will be the file
+		if (dup2(fd, STDOUT_FILENO) == -1) { // now the stdout will be the file
+			print_error_message_and_exit();
+		}
 		arglist[count - 2] = NULL; // override the ">>" sign with NULL because we do not pass this as an argument to execvp, and want to designate that this is the end of the first command
 		execute_command(arglist);
 	} else { // parent process
-		close(fd);
-		handle_waitpid(pid);
+		if (close(fd) == -1) {
+			print_error_message();
+			return 0;
+		}
+		if (handle_waitpid(pid, 0) == -1) { // waitpid failed
+			print_error_message();
+			return 0;
+		}
 	}
 	return 1;
 }
